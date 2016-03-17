8980f58a4943c7fdf58ddf5338aa8402ce92eba9
Ana Nora Evans <ananevans@gmail.com>
2016-03-17 14:28:28 -0400

    added

---
+++ Monad-Pole.hs
@@ -0,0 +1,17 @@
+type Birds = Int  
+type Pole = (Birds,Birds)
+
+
+landLeft :: Birds -> Pole -> Maybe Pole  
+landLeft n (left,right)  
+    | abs ((left + n) - right) < 4 = Just (left + n, right)  
+    | otherwise                    = Nothing  
+  
+landRight :: Birds -> Pole -> Maybe Pole  
+landRight n (left,right)  
+    | abs (left - (right + n)) < 4 = Just (left, right + n)  
+    | otherwise                    = Nothing
+
+
+main = do
+  putStrLn $ show $ landRight 1 (0,0) >>= landLeft 2
