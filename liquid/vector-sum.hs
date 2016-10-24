0b0820134b47bdd294076cf00ce93a65175d6926
Ana Nora Evans <ananevans@gmail.com>
2016-10-24 15:23:20 -0400

    Example from tutorial that does not work

---
+++ liquid/vector-sum.hs
@@ -0,0 +1,30 @@
+import Data.Vector
+
+vectorSum         :: Vector Int -> Int
+vectorSum vec     = go 0 5
+  where
+    go acc i
+      | i < sz    = go (acc + (vec ! i)) (i + 1)
+      | otherwise = acc
+    sz            = Data.Vector.length vec
+
+
+ -- /home/nora/work/learning-haskell/liquid/vector-sum.hs:7:25-39: Error: Liquid Type Mismatch
+ 
+ -- 7 |       | i < sz    = go (acc + (vec ! i)) (i + 1)
+ --                             ^^^^^^^^^^^^^^^
+ 
+ --   Inferred type
+ --     VV : {VV : Int | VV == acc + ?a
+ --                      && VV == ?b}
+  
+ --   not a subtype of Required type
+ --     VV : {VV : Int | VV < acc
+ --                      && VV >= 0}
+  
+ --   In Context
+ --     ?b : {?b : Int | ?b == acc + ?a}
+      
+ --     ?a : Int
+      
+ --     acc : Int
